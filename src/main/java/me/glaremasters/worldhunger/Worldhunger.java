40b3f8db6d69ecf58938f448f21e1fc701f45a4b
darbyjack <admin@glaremasters.me>
2018-09-25 01:10:18 -0500

    First commit

---
+++ src/main/java/me/glaremasters/worldhunger/Worldhunger.java
@@ -0,0 +1,29 @@
+package me.glaremasters.worldhunger;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Worldhunger extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    @EventHandler
+    public void onHungerChange(FoodLevelChangeEvent event) {
+        if (!(event.getEntity() instanceof Player)) return;
+        Player player = (Player) event.getEntity();
+        if (player.getFoodLevel() < 20) {
+            player.setFoodLevel(20);
+            player.sendMessage("Ending world hunger.... one event at a time! Enjoy your unlimited food!");
+        }
+    }
+}
